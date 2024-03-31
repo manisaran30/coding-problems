4a3e5024e1ca02c9d717616806b52842a017e09f
manisaran <101199986+manisaran30@users.noreply.github.com>
2024-03-31 12:04:05 +0530

    Create Two sum.cpp

---
+++ Two sum.cpp
@@ -0,0 +1,31 @@
+\\Two sum using brute force
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++){
+         for(int j=i+1;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+                return {i,j};
+            }
+         }
+        }
+        return {};
+    }
+};
+
+\\Two sum using hashing
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int n=nums.size();
+       unordered_map<int,int>numMap;
+       for(int i=0;i<n;i++){
+        int comp=target-nums[i];
+        if(numMap.count(comp)){
+            return{numMap[comp],i};
+        }
+        numMap[nums[i]]=i;
+       }
+       return {};
+       }
+};

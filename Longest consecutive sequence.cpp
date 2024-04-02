2c7f458f84700b43672031f2bab10aec43a08b03
manisaran <101199986+manisaran30@users.noreply.github.com>
2024-04-02 21:17:07 +0530

    Create Longest consecutive sequence.cpp

---
+++ Longest consecutive sequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+        
+        sort(nums.begin(), nums.end());
+        int maxlen = 1; 
+        int currlen = 1; 
+        
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                currlen++;
+            } else if (nums[i] !1= nums[i - 1]) {
+                maxlen = max(maxlen, currlen); 
+                currlen = 1;
+        }
+        }
+        
+        return max(maxlen, currlen); 
+        }
+};
